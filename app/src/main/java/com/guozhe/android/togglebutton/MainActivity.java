455942300e00076c2ffd7d46b72e701c3ecf6d50
KwakIron <guozhenzhu0517@gmail.com>
2017-09-12 16:53:21 +0900

    Initial commit

---
+++ app/src/main/java/com/guozhe/android/togglebutton/MainActivity.java
@@ -0,0 +1,29 @@
+package com.guozhe.android.togglebutton;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    ToggleButton toggleButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toggleButton = (ToggleButton)findViewById(R.id.toggleButton);
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(toggleButton.isChecked()){
+                    toggleButton.setTextColor(Color.GREEN);
+                }else {
+                    toggleButton.setTextColor(Color.WHITE);
+                }
+            }
+        });
+    }
+}
